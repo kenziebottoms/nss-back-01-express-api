6a24ee6dbdd964622958d1617e0a37f6892066dd
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-14 13:26:07 -0500

    also request director name at /movies endpoint

--- model/Movie.js
+++ model/Movie.js
@@ -5,9 +5,16 @@ const db = new sqlite3.Database("./db/mediaStore.sqlite");
 
 module.exports.getAll = () => {
     return new Promise((resolve, reject) => {
-        db.all(`SELECT * FROM movies`, (err, data) => {
-            if (err) return reject(err);
-            resolve(data);
-        });
+        db.all(`SELECT
+                    m.*,
+                    d.name as director
+                FROM movies m
+                JOIN
+                    directors d
+                    ON m.director_id = d.dir_id`,
+            (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
     });
 };
